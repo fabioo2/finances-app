5942c3f975727db4fa48c0402e6c6c45d04b5cd7
Fabio Kim <fabio.kim2@gmail.com>
2021-08-02 23:51:16 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config');
+const express = require('express');
+require('express-async-errors');
+const app = express();
+const cors = require('cors');
+
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+
+const middleware = require('./utils/middleware');
+
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
+
+app.use(middleware.unknownEndpoint);
+
+module.exports = app;
